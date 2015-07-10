1dd0497639efc161fb6e72802ad7b5c098f6e63b
Benoit VALLON <benoitvallon@gmail.com>
2015-07-10 11:34:40 -0400

    Changes the database schema

--- models/database.js
+++ models/database.js
@@ -1,7 +1,19 @@
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dataviewer';
 
 var client = new pg.Client(connectionString);
 client.connect();
-var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+
+// use the double quote to make the column names case-sensitive
+var query = client.query('CREATE TABLE bookmarks(' +
+  '"id" SERIAL PRIMARY KEY,' +
+  '"title" VARCHAR(1024) NOT NULL,' +
+  '"url" VARCHAR(1024) NOT NULL,' +
+  '"chromeDateAdded" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),' +
+  '"chromeId" BIGINT,' +
+  '"chromeParentId" BIGINT,' +
+  '"chromeIndex" BIGINT,' +
+  '"folder" VARCHAR(1024)' +
+')');
+
 query.on('end', function() { client.end(); });
