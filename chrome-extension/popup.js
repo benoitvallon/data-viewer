1ffeb2aeaa3deead1727a3eb5cf3d6b1765ae054
Benoit VALLON <benoitvallon@gmail.com>
2015-07-15 16:54:49 -0400

    Adds styles to the chrome extension

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,13 +1,24 @@
 // Listener
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('.button-extract').addEventListener('click', function () {
-    dumpBookmarks();
+$(document).ready(function () {
+  $('.button-extract').on('click', function () {
+    var button = $(this).button('loading')
+
+    getBookmarks(function() {
+      button.button('reset');
+      $('.button-extract').after(
+        '<div class="alert alert-success fade in">'+
+          '<button type="button" class="close" ' +
+            'data-dismiss="alert" aria-hidden="true">' + '&times;' +
+          '</button>' +
+          '<strong>Success!</strong> Bookmarks saved' +
+        '</div>');
+    });
   });
 });
 
-var flattenChromeBookmarksTree = function(chromeBookmarkTree) {
+var flattenBookmarksTree = function(bookmarkTree) {
   // for the moment only process "Bookmarks Bar" bookmarks
-  var treeToProcess = chromeBookmarkTree[0].children[0].children;
+  var treeToProcess = bookmarkTree[0].children[0].children;
   var flatTree = [];
 
   var browseTree = function(tree, parentTitle) {
@@ -39,21 +50,18 @@ var flattenChromeBookmarksTree = function(chromeBookmarkTree) {
   return flatTree;
 };
 
-// Traverse the bookmark tree, and print the folder and nodes.
-var dumpBookmarks = function(query) {
-  var bookmarkTreeNodes = chrome.bookmarks.getTree(
-    function(bookmarkTreeNodes) {
-      var bookmarks = flattenChromeBookmarksTree(bookmarkTreeNodes);
+// Browse the bookmark tree, and print the folder and nodes.
+var getBookmarks = function(callback) {
+  chrome.bookmarks.getTree(function(bookmarkTree) {
+    var bookmarks = flattenBookmarksTree(bookmarkTree);
 
-      $.ajax({
-        type: "POST",
-        url: 'http://localhost:3000/api/v1/bookmarks',
-        data: JSON.stringify(bookmarks),
-        contentType: 'application/json; charset=utf-8',
-        success: function(data) {
-          console.log('posted', data);
-        },
-        dataType: 'json'
-      });
+    $.ajax({
+      type: "POST",
+      url: 'http://localhost:3000/api/v1/bookmarks',
+      data: JSON.stringify(bookmarks),
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      success: callback
     });
+  });
 }
